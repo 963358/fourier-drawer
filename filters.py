118504c9bb7127a1eec997bcef331601bd1b174a
Benjamin Li <benjaminli@Benjamins-Laptop.local>
2023-12-11 23:14:51 -0800

    polar coordinate translation added

--- filters.py
+++ filters.py
@@ -3,6 +3,7 @@ from numpy import asarray
 import numpy
 import sys
 import matplotlib.pyplot as plt
+import math
 
 
 def start_filter(path):
@@ -17,15 +18,36 @@ def start_filter(path):
     # apply canny filter
     edged = cv2.Canny(blur, 150, 200)
 
-
     contours, hierarchy = cv2.findContours(edged,  
     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
     cv2.imshow('Canny Edges After Contouring', edged) 
    
     cropped = crop_image(edged, contours)
     cv2.imshow('Cropped Image', cropped)
-    
+
     cv2.waitKey(0) 
+
+    originX = cropped.shape[1]/2
+    originY = cropped.shape[0]/2
+
+    white_pixels = make_coordinates(cropped, originX, originY)
+    #print (white_pixels)
+
+    pixels_polar = make_polar(white_pixels)
+    print(pixels_polar)
+
+    x_array = []
+    y_array = []
+    for i in range(len(pixels_polar)):
+        r = pixels_polar[i][0]
+        cosine = pixels_polar[i][1]
+        sine = pixels_polar[i][2]
+        x_array.append(r*cosine)
+        y_array.append(r*sine)
+    
+    plt.plot(x_array, y_array, 'o')
+    plt.show()
+
 
 #numpydata = asarray(edged)
 #print(numpydata) 
@@ -47,28 +69,57 @@ def crop_image (edged, contours):
         maxY = max(maxY, y+h)
         minY = min(minY, y)
 
-        crop_img = edged[minY:maxY, minX:maxX]
-        return crop_img
+    crop_img = edged[minY:maxY, minX:maxX]
+    return crop_img
 
 
+def make_coordinates (cropped, originX, originY):
+    h = cropped.shape[0]
+    w = cropped.shape[1]
 
-# def make_coordinates (edged):
-#     white_pixels = []
-#     x_pixels = []
-#     y_pixels = []
-#     #make pixels into vectors relative to the origin
-#     for y in range(0, h):
-#         for x in range(0, w):
-#             # threshold the pixel
-#             if(crop_img[y, x]>0):
-#                 #cv2.circle(img, (x, y), radius=0, color=(0, 0, 255), thickness=-1)
-#                 # x_pixels.append(x-originX)
-#                 # y_pixels.append(originY-y)
+    white_pixels = []
+    x_pixels = []
+    y_pixels = []
+    #make pixels into vectors relative to the origin
+    for y in range(0, h):
+        for x in range(0, w):
+            # threshold the pixel
+            if(cropped[y, x]>0):
+                #cv2.circle(img, (x, y), radius=0, color=(0, 0, 255), thickness=-1)
+                x_pixels.append(x-originX)
+                y_pixels.append(originY-y)
 
-#                 arr = []
-#                 arr.append(x - originX)
-#                 arr.append(originY - y)
-#                 white_pixels.append(arr)
+                arr = []
+                arr.append(x - originX)
+                arr.append(originY - y)
+                white_pixels.append(arr)
+    
+    # plt.plot(x_pixels, y_pixels, 'o')
+    # plt.show()
+    
+    return white_pixels
+
+def make_polar (white_pixels):
+    length = len(white_pixels)
+    polar = []
+    for i in range (0, length):
+        x = white_pixels[i][0]
+        y = white_pixels[i][1]
+
+        arr = []
+        r = math.sqrt(x*x + y*y)
+        cosine = x/r
+        sine = y/r
+
+        arr.append(r)
+        arr.append(cosine)
+        arr.append(sine)
+        polar.append(arr)
+
+        #print(r, cosine, sine)
+    return polar
+
+    #print(white_pixels.shape[0], white_pixels.shape[1])
 
 #Find edges of contours
 # minX = 99999999
