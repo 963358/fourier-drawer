a3672621e4122dfd1725213a4bd6dec2267a992d
ryan <ryan963358@gmail.com>
2023-12-13 12:04:09 -0800

    updated file management

--- visuals.py
+++
@@ -1,9 +0,0 @@
-from manim import * 
-
-
-class visuals(Scene):
-    def construct(self):
-         circle = Circle()
-         circle.set_fill(PINK, opacity=0.5)
-         self.play(Create(circle))
-         self.wait()
